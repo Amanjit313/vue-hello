45f696c2e528e9a3479ec0b7b77c8a600e709427
Amanjit313 <amanjitsingh313@gmail.com>
2022-05-02 17:54:31 +0200

    Vue Hello - Bonus

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
+/* 
+
+- Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+- Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+*/
+
 const page = new Vue({
 
   el: "#page",
 
   data: {
-    messaggio: "Hello World!"
+    messaggio: "Hello World!",
+    immagine: "img/outrun-vaporwave-hd-wallpaper-preview.jpg"
   }
 })
